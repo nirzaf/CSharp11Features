52d3c548b5ade718b5584e188afedf58282064ce
Mohamed Fazrin <mfmfazrin1986@gmail.com>
2022-11-16 13:54:55 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CSharp11Features;
+
+using static System.Console;
+
+WriteLine("Testing new C# 11 Features...!");
+
+StringLiterals stringLiterals = new();
+WriteLine(stringLiterals.EscapeStringOld());
+WriteLine(stringLiterals.EscapeStringNew());
+
+WriteLine("Json As Text Old ");
+WriteLine(stringLiterals.JsonAsTextOld());
+WriteLine("Json As Text New ");
+WriteLine(stringLiterals.JsonAsTextNew());
+
+WriteLine("Json As Text with string interpolation new");
+WriteLine(stringLiterals.JsonWithStringInterpolationNew());
+
+
+ReadLine();
---
+++ StringLiterals.cs
@@ -0,0 +1,57 @@
+﻿namespace CSharp11Features;
+
+public class StringLiterals
+{
+    public string EscapeStringOld()
+    {
+        //Old way to escape a double quote in a string
+        return "<step number=\"3\">Connect A to B.</step>";
+    }
+
+    public string EscapeStringNew()
+    {
+        //New way to escape a double quote in a string
+        return """<step number="3">Connect A to B.</step>""";
+    }
+
+    public string JsonAsTextOld()
+    {
+        //Old way to create a string with a lot of double quotes
+        return "{ \"name\": \"John\", \"age\": 30, \"car\": null }";
+    }
+
+    public string JsonAsTextNew()
+    {
+        //New way to create a string with a lot of double quotes
+        return """
+        {
+        "menu": { 
+        "id": "file", 
+        "value": "File", 
+        "popup": {
+        "menuitem": [
+            {"value": "New", "onclick": "CreateNewDoc()"}, 
+            {"value": "Open", "onclick": "OpenDoc()"}, 
+            {"value": "Close", "onclick": "CloseDoc()"} 
+                ]}
+            }
+        }
+        """;
+    }
+
+    public string JsonWithStringInterpolationNew()
+    {
+        //New way to create a string with a lot of double quotes and string interpolation
+        string name = "John";
+        int age = 30;
+        string car = "BMW";
+        
+        return $$"""
+                {
+                "name": "{{name}}", 
+                "age": "{{age}}", 
+                "car": "{{car}}" 
+                }
+            """;
+    }
+}
