d6598846547258e2974dc7ccf1b179d38aef8f68
Mohamed Fazrin <mfmfazrin1986@gmail.com>
2022-11-18 15:08:42 +0300

    Designation

--- FileScopedClass/FileScopedClasses.cs
+++ FileScopedClass/FileScopedClasses.cs
@@ -3,12 +3,12 @@
 file class Employee
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string Address { get; set; }
-    public string Phone { get; set; }
-    public string Email { get; set; }
-    public string Department { get; set; }
-    public string Designation { get; set; }
+    public string? Name { get; set; }
+    public string? Address { get; set; }
+    public string? Phone { get; set; }
+    public string? Email { get; set; }
+    public string? Department { get; set; }
+    public string? Designation { get; set; }
 }
 
 
