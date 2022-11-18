62fde41885d46d589ae531b5d6f89fc230fe3248
Mohamed Fazrin <mfmfazrin1986@gmail.com>
2022-11-18 15:08:12 +0300

    Added File Scoped Class

---
+++ FileScopedClass/FileScopedClasses.cs
@@ -0,0 +1,15 @@
+﻿namespace CSharp11Features.FileScopedClass;
+
+file class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public string Department { get; set; }
+    public string Designation { get; set; }
+}
+
+
+
